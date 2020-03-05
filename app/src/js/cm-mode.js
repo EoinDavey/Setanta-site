1ee8d5e348a30b3935c3fb62fd766ae15e6c573c
Eoin Davey <eoind@vey.ie>
2020-03-05 22:09:56 +0000

    Update keyword highlighting for má, ma, nó, no
    Previous highlighting caused crashes on newlines

--- app/src/js/cm-mode.js
+++ app/src/js/cm-mode.js
@@ -15,7 +15,7 @@ CodeMirror.defineSimpleMode("setanta", {
     {regex: /(?:seo|tuis|le|bris|idir|chun-cinn|toradh|nuair-a)\b/,
      token: "keyword"},
     {regex: /(?:scr[ií]obh|fad|thar|cuid|mata|coladh|stáitse)\b/, token: "builtin"},
-    {regex: /(má|nó)([ {]|$)/, token: ["keyword", null]},
+    {regex: /(?:má|nó|no|ma)/, token: "keyword"},
     {regex: /f[ií]or|br[eé]ag|neamhn[ií]/, token: "atom"},
     {regex: /-?[0-9]+(?:\.[0-9]+)?/i,
      token: "number"},
