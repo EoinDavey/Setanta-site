838c94f3311b4f768c862b414be71b941cb6cda7
eoin <eoind@vey.ie>
2020-09-27 21:34:01 +0100

    Fix webpack config

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -22,17 +22,18 @@ module.exports = {
         publicPath: '/'
     },
     plugins: [
-        new CopyPlugin([
-            { from: '*.html', to: path.resolve(__dirname, 'dist/')},
-            { from: 'node_modules/codemirror/**/*.css', to: path.resolve(__dirname, 'dist')},
-            { from: "manifest.json", to: path.resolve(__dirname, 'dist/manifest.json') },
-            { from: "favicon.ico", to: path.resolve(__dirname, 'dist/favicon.ico') },
-            {
-                from: "node_modules/@webcomponents/webcomponentsjs/",
-                to: path.resolve(__dirname, "dist/node_modules/@webcomponents/webcomponentsjs/")
-            },
-            { from: "assets/", to: path.resolve(__dirname, "dist/assets") },
-            { from: "tutout/", to: path.resolve(__dirname, "dist/tut") }
-        ]),
+        new CopyPlugin({
+            patterns: [{ from: '*.html', to: path.resolve(__dirname, 'dist/')},
+                { from: 'node_modules/codemirror/**/*.css', to: path.resolve(__dirname, 'dist')},
+                { from: "manifest.json", to: path.resolve(__dirname, 'dist/manifest.json') },
+                { from: "favicon.ico", to: path.resolve(__dirname, 'dist/favicon.ico') },
+                {
+                    from: "node_modules/@webcomponents/webcomponentsjs/",
+                    to: path.resolve(__dirname, "dist/node_modules/@webcomponents/webcomponentsjs/")
+                },
+                { from: "assets/", to: path.resolve(__dirname, "dist/assets") },
+                { from: "tutout/", to: path.resolve(__dirname, "dist/tut") }
+            ]
+        }),
     ]
 };
