9e87c2a153dc0b840472c6260d3b83da0b80ff62
Eoin Davey <eoind@vey.ie>
2021-05-08 18:20:42 +0100

    Move TSC stage into Makefile

---
+++ app/webpack.new-build.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    watchOptions: {
+        ignored: /node_modules/
+    },
+    entry: {
+        editor: './buildfiles/tsc/fyp-app/fyp-app.js',
+        splash: './buildfiles/tsc/splash/splash.js',
+        mini: './buildfiles/tsc/mini/mini.js',
+        tut: './buildfiles/tsc/tut/tut.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'buildfiles/webpack')
+    },
+    devServer: {
+        contentBase: './buildfiles/webpack',
+        watchContentBase: true,
+        publicPath: '/'
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: '*.html', to: path.resolve(__dirname, 'buildfiles/webpack/')},
+                { from: 'node_modules/codemirror/**/*.css', to: path.resolve(__dirname, 'buildfiles/webpack')},
+                { from: "manifest.json", to: path.resolve(__dirname, 'buildfiles/webpack/manifest.json') },
+                { from: "favicon.ico", to: path.resolve(__dirname, 'buildfiles/webpack/favicon.ico') },
+                {
+                    from: "node_modules/@webcomponents/webcomponentsjs/",
+                    to: path.resolve(__dirname, "buildfiles/webpack/node_modules/@webcomponents/webcomponentsjs/")
+                },
+                { from: "assets/", to: path.resolve(__dirname, "buildfiles/webpack/assets") },
+                { from: "buildfiles/tut/", to: path.resolve(__dirname, "buildfiles/webpack/tut") }
+            ]
+        }),
+    ]
+};
